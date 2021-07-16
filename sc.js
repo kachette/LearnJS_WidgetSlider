6911b0bb42f25272e7d0e2ecce766da5ecdee27f
kachette <metelkinigor@gmail.com>
2021-07-16 15:43:20 +0100

    final commit

--- sc.js
+++ sc.js
@@ -1,3 +1,7 @@
+//('body').on("click mousedown mouseup focus blur keydown change dblclick mousemove mouseover mouseout mousewheel keydown keyup keypress textInput touchstart touchmove touchend touchcancel resize scroll zoom select change submit reset",function(e){
+//    console.log(e);
+//}); 
+
 const slider = document.querySelector('.slider-container'),
     slides = Array.from(document.querySelectorAll('.slide'))
 
@@ -24,16 +28,68 @@ slides.forEach((slide, index) => {
     slide.addEventListener('mousemove', touchMove)
 })
 
+
+// Disable context
+window.oncontextmenu = function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false;
+}
+
+
+
 function touchStart(index) {
     return function(event) {
-        console.log('start')
+        currentIndex = index;
+        startPos = getPositionX(event)
+        isDragging = true
+
+        animationId = requestAnimationFrame(animation)
+
+        slider.classList.add('grabbing')
     }
 }
 
 function touchEnd(event) {
-    console.log('end')
+    isDragging = false
+    cancelAnimationFrame(animationId)
+
+    const moveBy = currentTranslate - prevTranslate
+
+    if (moveBy < -100 && currentIndex < slides.length - 1)
+        currentIndex += 1
+
+    if (moveBy > 100 && currentIndex > 0) currentIndex -= 1
+
+    setPositionByIndex()
+
+    slider.classList.remove('grabbing')
 }
 
-function touchMove() {
-    console.log('move')
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event)
+        currentTranslate = prevTranslate + currentPosition -
+            startPos
+    }
+}
+
+function getPositionX(event) {
+    return event.type.includes('mouse') ? event.pageX :
+        event.touches[0].clientX;
+}
+
+function animation() {
+    setSliderPosition()
+    if (isDragging) requestAnimationFrame(animation)
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(${currentTranslate}px)`
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -window.innerWidth
+    prevTranslate = currentTranslate
+    setSliderPosition()
 }
